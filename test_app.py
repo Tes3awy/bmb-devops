853535a1cf91be4a0b2f2b858b8390ce10e0008b
Tes3awy <11368432+Tes3awy@users.noreply.github.com>
2026-09-06 14:42:12 +0300

    initial commit

---
+++ test_app.py
@@ -0,0 +1,19 @@
+from app import add_numbers, subtract_numbers, multiply_numbers
+
+
+def test_add_numbers():
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(0, 0) == 0
+    assert add_numbers(-1, -1) == -2
+
+
+def test_subtract_numbers():
+    assert subtract_numbers(3, 2) == 1
+    assert subtract_numbers(10, 10) == 0
+    assert subtract_numbers(-1, -1) == 0
+
+
+def test_multiply_numbers():
+    assert multiply_numbers(3, 2) == 6
+    assert multiply_numbers(0, 0) == 0
+    assert multiply_numbers(-1, -1) == 1
